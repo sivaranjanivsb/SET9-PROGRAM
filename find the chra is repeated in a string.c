ba891cadb7a049cbcbbbc9e29cc265c7571cc2ac
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-03-01 11:12:46 +0000

    Update find the chra is repeated in a string.c

--- find the chra is repeated in a string.c
+++ find the chra is repeated in a string.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include<string.h>
-int main(void){
-	char a[50];
-	int s,i,c=0;
+int main(void) {
+	char a[10];
+	int i,s,j,c=0;
 	scanf("%s",a);
 	s=strlen(a);
-	printf("%d",s);
-	for(i=0;i<=s;i++)
+	printf("%d\n",s);
+	for(i=0;i<s;i++)
 	{
-	    if(a[i]==a[i])
-	    {
-	        c++;
-	    }
-	   
+		for(j=i+1;j<=s;j++)
+		{
+	if(a[i]==a[j]){
+	c++;}
 	}
-	if(c==0){
-	printf("yes");}
-	else{
-	printf("no");}
+	}
+	if(c==0)
+	printf("yes");
+	else
+	printf("no");
 	return 0;
 }
