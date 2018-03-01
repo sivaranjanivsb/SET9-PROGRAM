a7cd1c11cce18ef76c0131ad9e45118e8caadd96
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-03-01 10:37:11 +0000

    Update odd and even positions.c

--- odd and even positions.c
+++ odd and even positions.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include<string.h>
 int main(void){
-	char a[50];
-	int i,l;
+	char a[50],b[10],c[10];
+	int i,l,k=1,j=i;
 	scanf("%s",a);
 	l=strlen(a);
 	printf("%d",l);
-	for(i=1;i<=l;i++)
+	for(i=0;i<=l-1;i++)
 	{
-		if(i%2==0){
-		printf("%c",a[i]);}
+		if(i%2==0)
+		{
+		    b[k]=a[i];
+		    k++;
+        }
 		else
-		{printf("\n%c",a[i]);}
+		{
+		    c[j]=a[i];
+		    j++;
+	     }
+	     
 	}
+	printf("\n%s",b);
+	printf("\n%s",c);
 	return 0;
 }
